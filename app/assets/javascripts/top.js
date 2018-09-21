a2ed37af5f195278b87e2192bcc9c5f9d108b38c
EC2 Default User <ec2-user@ip-172-31-11-29.ap-southeast-1.compute.internal>
2018-09-21 14:52:26 +0000

    jQuery

--- app/assets/javascripts/top.js
+++ app/assets/javascripts/top.js
@@ -1,16 +1,64 @@
-window.addEventListener("load", function() {
-  // windowオブジェクトにloadイベントが起きたらfunction実行
-  var btn = document.querySelector("input");
-  // 変数定義。documentオブジェクトのinputタグを取得しbtnに代入
-  
-  btn.addEventListener("click", function() {
-    // btnにclickイベントが起きたらfunction実行
-    var p = document.querySelector("p");
-    // 変数定義。documentオブジェクトのpタグを取得しpに代入
-    p.innerText = "クリックされた！";
-    // pのinnerTextを”くりっくされた！”に書き換える
+$(window).on("load", function() {
+  $("li").on("click", function() {
+    $("li.selected").removeClass("selected");
+    $(this).addClass("selected");
+    $(".contents div").hide();
+    $("."+this.id).show();
   });
 });
+
+
+
+// window.addEventListener("load", function() {
+//     windowオブジェクトにloadイベントが起きたら関数実行
+//   $("li").on("click", function() {
+//     liタグがclickされたら関数実行
+//     $("li.selected").removeClass("selected");
+//     liタグのselectedクラスからselectedクラスを除く
+//     $(this).addClass("selected");
+//     イベントが発生した要素にselectedクラス付け足す
+//     $(".contents div").hide();
+//     contentsクラス配下のdivタグを消す
+//     $("." + this.id).show();
+//     イベント発生した要素のidと同じ名前のクラスのタグを表示
+//   });
+// });
+
+
+// window.addEventListener("load", function() {
+  // windowオブジェクトにloadイベントが起きたら関数実行
+  // $("li").on("click", function() {
+    // liタグがclickされたら関数実行
+    // $("li.selected").removeClass("selected");
+    // liタグのselectedクラスからselectedクラスを除く
+    // $(this).addClass("selected");
+    // イベントが発生した要素にselectedクラス付け足す
+//   });
+// });
+
+
+// window.addEventListener("load", function() {
+//   windowオブジェクトがloadされたら関数実行
+//   $("li").on("click", function() {
+//     liタグがクリックされたら関数実行
+//     console.log(this.id);
+//     consolelogにクリックされたタグのid表示
+//   });
+// });
+
+// window.addEventListener("load", function() {
+  // windowオブジェクトにloadイベントが起きたらfunction実行
+  // var btn = document.querySelector("input");
+  // 変数定義。documentオブジェクトのinputタグを取得しbtnに代入
+  
+  // btn.addEventListener("click", function() {
+    // btnにclickイベントが起きたらfunction実行
+    // var p = document.querySelector("p");
+    // 変数定義。documentオブジェクトのpタグを取得しpに代入
+    // p.innerText = "クリックされた！";
+    // pのinnerTextを”くりっくされた！”に書き換える
+//   });
+// });
 
 
 // window.addEventListener("load", function() {
